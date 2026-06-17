a89eb28b7b165fb7cc8cd953b6a2214e14c3290c
KirillMatviychuck <kirill.matviychuck@gmail.com>
2026-06-17 10:20:50 +0300

    add jest, add first test

---
+++ src/helpers/classNames/classNames.test.ts
@@ -0,0 +1,20 @@
+import { classNames } from "./classNames";
+
+describe('classNames', () => {
+
+    test('only with first param', () => {
+        expect(classNames('someClass')).toBe('someClass');
+    });
+    test('with additional class', () => {
+        expect(classNames('someClass', {}, ['class1', 'class2'])).toBe('someClass class1 class2');
+    });
+    test('with mods', () => {
+        const expected = 'someClass class1 class2 hovered scrollable'
+        expect(classNames('someClass', { hovered: true, scrollable: true }, ['class1', 'class2'])).toBe(expected);
+    });
+
+    test('with mods', () => {
+        const expected = 'someClass class1 class2 hovered'
+        expect(classNames('someClass', { hovered: true, scrollable: false }, ['class1', 'class2'])).toBe(expected);
+    });
+});
